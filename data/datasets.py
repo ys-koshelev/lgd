34c9e8c32d9126f562705053814bfa6731249f69
ys-koshelev <Iaroslav.Koshelev@skoltech.ru>
2020-10-22 17:51:04 +0300

    Implemented datasets, based on ADE20K

--- data/datasets.py
+++ data/datasets.py
@@ -2,27 +2,50 @@ import numpy as np
 
 from forked.SPADE.data.ade20k_dataset import ADE20KDataset
 from .kernels import GaussianKernelSampler, ShakeKernelSampler
-
+from argparse import Namespace
 
 class NoiseDataset(ADE20KDataset):
     """
     Wrapper, which adds noise stds to dataset
     """
-    def __init__(self, min_std: float, max_std: float, dataset_root: str) -> None:
+    def __init__(self, min_std: float, max_std: float, dataset_root: str, train_phase: str = 'train',
+                 max_dataset_length: int = 1000, out_images_size: int = 256) -> None:
         """
         :param min_std: lower bound value of noise standard deviation to use in degradation
         :param max_std: upper bound value of noise standard deviation to use in degradation
         :param dataset_root: path to dataset root folder
+        :param train_phase: should be either train, val or test
+        :param max_dataset_length: maximum length of dataset
+        :param out_images_size: size of images to be outputed by this dataset
         """
         self.min_std = min_std
         self.max_std = max_std
-        self.initialize({'dataroot': dataset_root})
+        opts = self.get_options(dataset_root, train_phase, max_dataset_length, out_images_size)
+        self.initialize(opts)
 
     def __getitem__(self, item):
         data = super().__getitem__(item)
         noise_std = self.min_std + np.random.rand()*(self.max_std - self.min_std)
         data.update({'noise_std': noise_std})
         return data
+
+    def get_options(self, root_dir: str, phase: str, max_size: int, crop_size: int) -> Namespace:
+        options = Namespace()
+        options.dataroot = root_dir
+        options.phase = phase
+        options.no_instance = True
+        options.max_dataset_size = max_size
+        options.no_pairing_check = True
+        options.preprocess_mode = 'crop'
+        options.crop_size = crop_size
+        if phase == 'train':
+            is_train = True
+        else:
+            is_train = False
+        options.isTrain = is_train
+        options.no_flip = False
+        options.label_nc = 150
+        return options
 
 
 class DownscalingDataset(NoiseDataset):
