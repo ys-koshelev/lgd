445bab957127ad21f64944b79085a16d292c8dbf
ys-koshelev <Iaroslav.Koshelev@skoltech.ru>
2020-10-29 11:03:53 +0300

    use_gradlikelihood_input is now an LGD class attribute

--- optimizers/learnable.py
+++ optimizers/learnable.py
@@ -10,14 +10,17 @@ class LearnedGradientDescentOptimizer(nn.Module, OptimizerBase):
     """
     Class which implements learnable gradient descent optimizer as a recurrent convolutional neural network.
     """
+    degradation: DegradationBase
     backbone: nn.Module
+    num_steps: int
     backprop_through_gradlikelihood: bool
     hidden_state_num_channels: int
+    use_gradlikelihood_input: bool
 
     def __init__(self, degradation: DegradationBase, optimizer_network: nn.Module, num_steps: int,
                  projection_function: Callable = None, backprop_through_gradlikelihood: bool = False,
                  hidden_state_network: nn.Module = None, device: Union[th.device, str] = 'cpu',
-                 hidden_state_num_channels: int = None) -> None:
+                 hidden_state_num_channels: int = None, use_gradlikelihood_input: bool = True) -> None:
         """
         Initialize a learnable gradient descent optimizer
 
@@ -28,12 +31,14 @@ class LearnedGradientDescentOptimizer(nn.Module, OptimizerBase):
         :param hidden_state_network: neural network used to calculate running hidden state of lgd optimizer
         :param device: device to run optimization and training
         :param hidden_state_num_channels: number of output channels of hidden state network
+        :param use_gradlikelihood_input: if True then likelihood gradients are passed to backbone with current estimates
         """
         super().__init__()
         self.degradation = degradation
         self.backbone = optimizer_network.to(device=device)
         self.num_steps = num_steps
         self.backprop_through_gradlikelihood = backprop_through_gradlikelihood
+        self.use_gradlikelihood_input = use_gradlikelihood_input
         if projection_function is None:
             self.projection_function = IdentityProjection()
         else:
@@ -44,18 +49,17 @@ class LearnedGradientDescentOptimizer(nn.Module, OptimizerBase):
             self.hidden_state_backbone = self.hidden_state_backbone.to(device=device)
             self.hidden_state_num_channels = hidden_state_num_channels
 
-    def perform_step(self, latent_images: th.Tensor, degraded_images: th.Tensor, hidden_state: th.Tensor = None,
-                     use_gradlikelihood_input: bool = True) -> th.Tensor:
+    def perform_step(self, latent_images: th.Tensor, degraded_images: th.Tensor,
+                     hidden_state: th.Tensor = None) -> th.Tensor:
         """
         This method performs a minimization step on objective.
 
         :param latent_images: batch of latent images of shape [B, C1, H1, W1], which should be updated
         :param degraded_images: batch of degraded images of shape [B, C2, H2, W2]
         :param hidden_state: batch of hidden states of shape [B, Ch. H1, W1] to use in calculating update step
-        :param use_gradlikelihood_input: if True then likelihood gradients are passed to backbone with current estimates
         :return: updated images of shape [B, C, H, W]
         """
-        if use_gradlikelihood_input:
+        if self.use_gradlikelihood_input:
             input_gradients = self.degradation.grad_likelihood(degraded_images, latent_images,
                                                                self.backprop_through_gradlikelihood)
             assert input_gradients.shape == latent_images.shape
@@ -71,15 +75,13 @@ class LearnedGradientDescentOptimizer(nn.Module, OptimizerBase):
         update_step = self.backbone(network_input)
         return latent_images + update_step, hidden_state
 
-    def restore(self, degraded_images: th.Tensor, track_updates_history: bool = False,
-                use_gradlikelihood_input: bool = True) -> Union[List[th.Tensor], th.Tensor]:
+    def restore(self, degraded_images: th.Tensor, track_updates_history: bool = False) -> Union[List[th.Tensor], th.Tensor]:
         """
         This method performs restoration of input degraded images.
 
         :param degraded_images: batch of degraded images of shape [B, C, H, W] needed for restoration
         :param track_updates_history: if set to True, latent images after each step are not discarded
         and a list of images from all updates is returned instead of last latent estimate
-        :param use_gradlikelihood_input: if True then likelihood gradients are passed to backbone with current estimates
         :return: restored images of shape [B, C, H, W]
         """
         if track_updates_history:
@@ -91,7 +93,7 @@ class LearnedGradientDescentOptimizer(nn.Module, OptimizerBase):
         else:
             hidden_state = None
         for i in range(self.num_steps):
-            if use_gradlikelihood_input:
+            if self.use_gradlikelihood_input:
                 latent_images, hidden_state = self.perform_step(latent_images, degraded_images, hidden_state)
             else:
                 latent_images, hidden_state = \
